911985a87b1de459d5558bc443bcc5dac6bb5004
jetgenobia <guinocorgadjet17@gmail.com>
2025-08-09 14:22:28 +0800

    polish views

---
+++ Controllers/MentorshipManageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Freelancing.Controllers
+{
+    public class MentorshipManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
