575aa098c79b94eb90efe7a6881f7c3ef903ea4e
jetgenobia <guinocorgadjet17@gmail.com>
2025-08-15 22:18:59 +0800

    fix view

---
+++ Controllers/DeliverableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Freelancing.Controllers
+{
+    public class DeliverableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
