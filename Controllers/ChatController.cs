c3a3c70cdb4ab0301e0a2528ba2e3d6345da5a8d
jetgenobia <guinocorgadjet17@gmail.com>
2025-08-16 12:58:12 +0800

    added message

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Freelancing.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
